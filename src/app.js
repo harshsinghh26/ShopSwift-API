fea12e6a1055a69fb07d728e8c23684a191e7bfa
harshsinghh26 <harshsi343@gmail.com>
2025-04-12 17:56:29 +0530

    set routes and cors url for frontend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_URL,
+    credentials: true,
+  }),
+);
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '16kb' }));
+app.use(express.static('public'));
+app.use(cookieParser());
+
+// Import route of users
+
+import Userouter from './routes/users.routes.js';
+
+app.use('/api/v1/users', Userouter);
 
 export default app;
