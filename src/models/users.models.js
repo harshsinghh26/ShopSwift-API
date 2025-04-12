a009042ef2a6bc3aca26bd31f6acc450cc9ceee9
harshsinghh26 <harshsi343@gmail.com>
2025-04-12 17:51:59 +0530

    added save password in database in hash format also check password and generate access and refersh token function

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema(
   {
@@ -32,5 +34,47 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 );
+
+// Save password in Hash Format
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Check Password is correct
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// generate Access Token
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+// Generate Refresh Web Token
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
 
 export const User = mongoose.model('User', userSchema);
