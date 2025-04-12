9f92df3b28d6bc12958eef40eb9529e28604a02c
harshsinghh26 <harshsi343@gmail.com>
2025-04-12 17:30:17 +0530

    added image uplading function

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,18 @@
+import multer from 'multer';
+
+const storageDisk = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const allowdType = ['image/png', 'image/jpeg'];
+
+    if (!allowdType.includes(file.mimetype)) {
+      return cb(new ApiError(401, 'unsupported Format!!'));
+    }
+    return cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = `${Date.now()}-${file.originalname}`;
+    return cb(null, uniqueName);
+  },
+});
+
+export const upload = multer({ storageDisk });
